18143255e0deada7c2d2374af81fe53d758bffba
Aarthiganapathy <aarthiradhag@gmail.com>
2025-03-14 11:58:46 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
+Route::get('/', function () {
+    return redirect('/tasks');
+});
+
 
 
 Route::resource('tasks', TaskController::class);
